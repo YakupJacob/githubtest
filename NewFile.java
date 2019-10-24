6f9b5d066704d90a362e381a726f99da008d14ad
YakupJacob <56706894+YakupJacob@users.noreply.github.com>
2019-10-24 13:25:18 -0400

    I am putting any thing
    new comment

---
+++ NewFile.java
@@ -0,0 +1,25 @@
+class program
+package jacob;
+
+public class array {
+    public static void main(String[] arg){
+      int test = 9;
+      if (test == 9) {
+          System.out.println("Input your age");
+      }else if(test < 9) {
+          System.out.println("this is else");
+      }else {
+          System.out.println("yes");
+      }
+     }
+}
+//    String[] newArr = new String[5];
+//        newArr[0]= "hello";
+//        newArr[1]= "hi";
+//        newArr[2]= "tim";
+//        newArr[3]= "bill";
+//        newArr[4]= "joe";
+//
+//        int[] nums = {2,3,54,6,7};
+//        int x = nums[2];
+//     System.out.println(x);
